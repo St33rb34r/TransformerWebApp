7bdbc804e5b573256edebb303c71993d360ea6d6
markus hector <markus.hector@visigon.com>
2024-04-08 18:25:01 +0200

    Changed dockerfile

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 
-#from routers import model
-
 
 app = FastAPI()
-
-#app.include_router(model.router)
 
 
 @app.get("/")
--- app/routers/model.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/model/health")
-def model_health():
-    return {"status": "running"}
