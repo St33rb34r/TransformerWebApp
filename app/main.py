0ed57c7f789a58c24fee8a1ad1545cf649fd8b20
markus hector <markus.hector@visigon.com>
2024-04-21 18:38:37 +0200

    Changed name of translation

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from .router import transformers, sentiment
+from app.router import translation, sentiment
 
 
 app = FastAPI(root_path='/prod')    # To make /docs accessible from AWS
 
-app.include_router(transformers.router)
+app.include_router(translation.router)
 app.include_router(sentiment.router)
 
 
